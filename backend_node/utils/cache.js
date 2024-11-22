cd6ad0cd82b6614509e81ce6f1c995ebabdac0fa
TheUzair <theuzair_email@example.com>
2024-11-22 15:16:23 +0530

    Fix Redis configuration

--- backend_node/utils/cache.js
+++ backend_node/utils/cache.js
@@ -1,7 +1,11 @@
 import { createClient } from 'redis';
 
 const redisClient = createClient({
-  url: `redis://:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT || 6379}`,
+  password: process.env.REDIS_PASSWORD, 
+  socket: {
+    host: process.env.REDIS_HOST, 
+    port: parseInt(process.env.REDIS_PORT, 10) || 6379,
+  },
 });
 
 // Handle Redis connection errors
